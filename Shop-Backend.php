dc5785ad799148792806bb7365760b1e2ca4e91d
ehsan-masoudipour <ehsan89ma@gmail.com>
2026-08-24 22:48:39 +0330

    Shop-PROJECT
    The first practice project for functions alongside foreach loops in PHP. This project was to practice the functions that were seen as introductory training...

--- Shop-Backend.php
+++ Shop-Backend.php
@@ -116,8 +116,8 @@ function renderStars($rating) {
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.rtl.min.css"
         rel="stylesheet"
     >
-    <link rel="stylesheet" href="Shop.css">
-</head>
+    <link rel="stylesheet" href="Shop-Backend.css">
+</head>Shop-Backend.css
 <body>
 <!-- =====================================================
     TOP BAR
